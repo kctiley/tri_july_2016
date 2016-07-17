710732571597c363b0cce67c50a9c9aa7e2276bc
Klint Tiley <kctiley@gmail.com>
2016-07-16 23:24:30 -0600

    Init commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,21 @@
+
+var app = angular.module('tileyRoofing', ['ui.router', 'ngRoute', 'ui.bootstrap', 'ngAnimate', 'ngTouch'])
+  .config( function($stateProvider, $urlRouterProvider, $locationProvider){
+
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider
+      .state('/estimate_form', {
+        url: "/estimate_form",
+        templateUrl: "partials/estimate_form.html",
+        controller: "homeController"
+      })
+      .state('/', {
+        url: "/",
+        templateUrl: "partials/home.html",
+        controller: "homeController"
+      })
+
+
+
+})
